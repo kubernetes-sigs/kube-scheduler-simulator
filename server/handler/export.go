781fd9669fc25c8f783528caed84d840500d1393
196Ikuchil <196Ikuchil@gmail.com>
2021-12-21 01:55:52 +0900

    fix:remove resotre process on import

--- server/handler/export.go
+++ server/handler/export.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/kubernetes-sigs/kube-scheduler-simulator/export"
@@ -32,13 +31,6 @@ func (h *ExportHandler) Export(c echo.Context) error {
 func (h *ExportHandler) Import(c echo.Context) error {
 	ctx := c.Request().Context()
 
-	// backup before overwrite exist resources.
-	bkp, err := h.service.Export(ctx)
-	if err != nil {
-		klog.Errorf("failed to backup resources before import: %+v", err)
-		return echo.NewHTTPError(http.StatusInternalServerError)
-	}
-
 	reqResources := new(export.ResourcesApplyConfiguration)
 	if err := c.Bind(reqResources); err != nil {
 		klog.Errorf("failed to bind import resources all request: %+v", err)
@@ -48,22 +40,7 @@ func (h *ExportHandler) Import(c echo.Context) error {
 	rs, err := h.service.Import(ctx, reqResources)
 	if err != nil {
 		klog.Errorf("failed to import all resources: %+v", err)
-		// revocery step from backup resources
-		bkpResources := new(export.ResourcesApplyConfiguration)
-		bkpr, err1 := json.Marshal(bkp)
-		if err1 != nil {
-			klog.Errorf("failed to parse json of recovery resources: %+v", err1)
-			return echo.NewHTTPError(http.StatusInternalServerError)
-		}
-		if err2 := json.Unmarshal(bkpr, &bkpResources); err2 != nil {
-			klog.Errorf("failed to convert to ResourcesApplyConfiguration of recovery resources: %+v", err2)
-			return echo.NewHTTPError(http.StatusInternalServerError)
-		}
-		_, err3 := h.service.Import(ctx, bkpResources)
-		if err3 != nil {
-			klog.Errorf("failed to recover of backup resources: %+v", err3)
-			return echo.NewHTTPError(http.StatusInternalServerError)
-		}
+		return echo.NewHTTPError(http.StatusInternalServerError)
 	}
 	return c.JSON(http.StatusOK, rs)
 }
