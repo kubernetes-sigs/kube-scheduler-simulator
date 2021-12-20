e630aa831966f6f945098e1cf7e7f3141a762833
sivchari <shibuuuu5@gmail.com>
2021-12-21 02:13:26 +0900

    add delete all test of node

--- node/mock_node/node.go
+++ node/mock_node/node.go
@@ -49,6 +49,20 @@ func (mr *MockPodServiceMockRecorder) Delete(arg0, arg1 interface{}) *gomock.Cal
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Delete", reflect.TypeOf((*MockPodService)(nil).Delete), arg0, arg1)
 }
 
+// DeleteAll mocks base method.
+func (m *MockPodService) DeleteAll(arg0 context.Context) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "DeleteAll", arg0)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// DeleteAll indicates an expected call of DeleteAll.
+func (mr *MockPodServiceMockRecorder) DeleteAll(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeleteAll", reflect.TypeOf((*MockPodService)(nil).DeleteAll), arg0)
+}
+
 // List mocks base method.
 func (m *MockPodService) List(arg0 context.Context) (*v1.PodList, error) {
 	m.ctrl.T.Helper()
--- node/node_test.go
+++ node/node_test.go
@@ -146,3 +146,122 @@ func TestService_Delete(t *testing.T) {
 		})
 	}
 }
+
+func TestService_DeleteAll(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name                    string
+		preparePodServiceMockFn func(m *mock_node.MockPodService)
+		prepareFakeClientSetFn  func() *fake.Clientset
+		wantErr                 bool
+	}{
+		{
+			name: "delete all nodes and pods on nodes",
+			preparePodServiceMockFn: func(m *mock_node.MockPodService) {
+				m.EXPECT().List(gomock.Any()).Return(&corev1.PodList{
+					Items: []corev1.Pod{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: "pod1",
+							},
+							Spec: corev1.PodSpec{
+								NodeName: "node1",
+							},
+						},
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: "pod2",
+							},
+							Spec: corev1.PodSpec{
+								NodeName: "node1",
+							},
+						},
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: "other-pod1",
+							},
+							Spec: corev1.PodSpec{
+								NodeName: "node2",
+							},
+						},
+					},
+				}, nil)
+				m.EXPECT().DeleteAll(gomock.Any()).Return(nil)
+			},
+			prepareFakeClientSetFn: func() *fake.Clientset {
+				c := fake.NewSimpleClientset()
+				// add test data.
+				c.CoreV1().Nodes().Create(context.Background(), &corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "node1",
+					},
+				}, metav1.CreateOptions{})
+				return c
+			},
+			wantErr: false,
+		},
+		{
+			name: "failt to delete no nodes",
+			preparePodServiceMockFn: func(m *mock_node.MockPodService) {
+				m.EXPECT().List(gomock.Any()).Return(&corev1.PodList{
+					Items: []corev1.Pod{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: "pod1",
+							},
+							Spec: corev1.PodSpec{
+								NodeName: "node1",
+							},
+						},
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: "pod2",
+							},
+							Spec: corev1.PodSpec{
+								NodeName: "node1",
+							},
+						},
+					},
+				}, nil)
+				m.EXPECT().DeleteAll(gomock.Any()).Return(errors.New("error"))
+			},
+			prepareFakeClientSetFn: func() *fake.Clientset {
+				c := fake.NewSimpleClientset()
+				return c
+			},
+			wantErr: true,
+		},
+		{
+			name: "delete node with no pods",
+			preparePodServiceMockFn: func(m *mock_node.MockPodService) {
+				m.EXPECT().List(gomock.Any()).Return(&corev1.PodList{Items: []corev1.Pod{}}, nil)
+			},
+			prepareFakeClientSetFn: func() *fake.Clientset {
+				c := fake.NewSimpleClientset()
+				// add test data.
+				c.CoreV1().Nodes().Create(context.Background(), &corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "node1",
+					},
+				}, metav1.CreateOptions{})
+				return c
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			ctrl := gomock.NewController(t)
+			mockPodService := mock_node.NewMockPodService(ctrl)
+			tt.preparePodServiceMockFn(mockPodService)
+			fakeclientset := tt.prepareFakeClientSetFn()
+
+			s := NewNodeService(fakeclientset, mockPodService)
+			if err := s.DeleteAll(context.Background()); (err != nil) != tt.wantErr {
+				t.Fatalf("DeleteAll() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
