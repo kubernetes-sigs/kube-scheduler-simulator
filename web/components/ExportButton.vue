2d171ae2da97660c9141a6580e6172ab07befaa3
196Ikuchil <196thinline@gmail.com>
2022-02-03 20:40:43 +0900

    add: export/import button components

---
+++ web/components/ExportButton.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-dialog v-model="data.dialog" width="500">
+    <template #activator="{ on }">
+      <v-btn color="primary ma-2" v-on="on"> Export </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2"> Export </v-card-title>
+
+      <v-card-text> Export the current scheduler. </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="green darken-1" text @click="data.dialog = false">
+          Cancel
+        </v-btn>
+        <v-btn color="green darken-1" text @click="ExportScheduler()">
+          Export
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { exportScheduler } from "../api/v1/export";
+import { saveAs } from "file-saver";
+import { defineComponent, inject, reactive } from "@nuxtjs/composition-api";
+import SnackBarStoreKey from "./StoreKey/SnackBarStoreKey";
+import yaml from "js-yaml";
+
+export default defineComponent({
+  setup() {
+    const data = reactive({
+      dialog: false,
+    });
+    const snackbarstore = inject(SnackBarStoreKey);
+    if (!snackbarstore) {
+      throw new Error(`${SnackBarStoreKey} is not provided`);
+    }
+    const setServerErrorMessage = (error: string) => {
+      snackbarstore.setServerErrorMessage(error);
+    };
+
+    async function ExportScheduler() {
+      try {
+        const c = await exportScheduler();
+        if (c) {
+          const blob = new Blob([yaml.dump(c)], {
+            type: "application/yaml",
+          });
+          saveAs(blob, "export.yml");
+          data.dialog = false;
+        }
+      } catch (e) {
+        setServerErrorMessage(e);
+      }
+    }
+    return {
+      data,
+      ExportScheduler,
+    };
+  },
+});
+</script>
---
+++ web/components/ImportButton.vue
@@ -0,0 +1,134 @@
+<template>
+  <v-dialog v-model="data.dialog" width="500">
+    <template #activator="{ on }">
+      <v-btn color="primary ma-2" v-on="on"> Import </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2"> Import </v-card-title>
+
+      <v-card-text>
+        ※WARNING: Scheduler will be initialized before importing
+        <form>
+          <input type="file" accept=".yml" @change="readfile" />
+        </form>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="green darken-1" text @click="data.dialog = false">
+          Cancel
+        </v-btn>
+        <v-btn
+          color="green darken-1"
+          text
+          :disabled="data.is_import_button_disabled"
+          @click="ImportScheduler()"
+        >
+          Import
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject, reactive } from "@nuxtjs/composition-api";
+import { importScheduler, ResourcesForImport } from "../api/v1/export";
+import yaml from "js-yaml";
+import SnackBarStoreKey from "./StoreKey/SnackBarStoreKey";
+import PriorityClassStoreKey from "./StoreKey/PriorityClassStoreKey";
+import StorageClassStoreKey from "./StoreKey/StorageClassStoreKey";
+import PersistentVolumeClaimStoreKey from "./StoreKey/PVCStoreKey";
+import PersistentVolumeStoreKey from "./StoreKey/PVStoreKey";
+import NodeStoreKey from "./StoreKey/NodeStoreKey";
+import PodStoreKey from "./StoreKey/PodStoreKey";
+
+interface SelectedItem {
+  dialog: boolean;
+  filedata: ResourcesForImport;
+  is_import_button_disabled: boolean;
+}
+
+export default defineComponent({
+  setup() {
+    const data = reactive({
+      dialog: false,
+      is_import_button_disabled: true,
+    } as SelectedItem);
+    const priorityclassstore = inject(PriorityClassStoreKey);
+    if (!priorityclassstore) {
+      throw new Error(`${PriorityClassStoreKey} is not provided`);
+    }
+    const storageclassstore = inject(StorageClassStoreKey);
+    if (!storageclassstore) {
+      throw new Error(`${StorageClassStoreKey} is not provided`);
+    }
+    const pvcstore = inject(PersistentVolumeClaimStoreKey);
+    if (!pvcstore) {
+      throw new Error(`${PersistentVolumeClaimStoreKey} is not provided`);
+    }
+    const pvstore = inject(PersistentVolumeStoreKey);
+    if (!pvstore) {
+      throw new Error(`${PersistentVolumeStoreKey} is not provided`);
+    }
+    const nstore = inject(NodeStoreKey);
+    if (!nstore) {
+      throw new Error(`${NodeStoreKey} is not provided`);
+    }
+    const pstore = inject(PodStoreKey);
+    if (!pstore) {
+      throw new Error(`${PodStoreKey} is not provided`);
+    }
+
+    const snackbarstore = inject(SnackBarStoreKey);
+    if (!snackbarstore) {
+      throw new Error(`${SnackBarStoreKey} is not provided`);
+    }
+    const setServerErrorMessage = (error: string) => {
+      snackbarstore.setServerErrorMessage(error);
+    };
+    const ImportScheduler = async () => {
+      importScheduler(data.filedata as ResourcesForImport)
+        .then(() => {
+          priorityclassstore.fetchlist();
+          storageclassstore.fetchlist();
+          pvcstore.fetchlist();
+          pvstore.fetchlist();
+          nstore.fetchlist();
+          pstore.fetchlist();
+        })
+        .catch((e) => setServerErrorMessage(e))
+        .finally(() => {
+          data.dialog = false;
+        });
+    };
+    function readfile(e: { target: { files: FileList | null } }) {
+      if (e.target.files === null) return;
+      const file = e.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = function () {
+        try {
+          const filedate: ResourcesForImport = yaml.load(
+            reader.result as string
+          );
+          data.filedata = filedate;
+          data.is_import_button_disabled = false;
+        } catch (e) {
+          setServerErrorMessage("Failed to load the selected file.");
+        }
+      };
+      reader.readAsText(file);
+    }
+
+    return {
+      data,
+      ImportScheduler,
+      readfile,
+    };
+  },
+});
+</script>
