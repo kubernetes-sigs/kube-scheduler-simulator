9e60188e98966dfc91b938f76e1474f5ecf77a38
sivchari <shibuuuu5@gmail.com>
2022-02-12 01:56:43 +0900

    delete client

--- reset/reset.go
+++ reset/reset.go
@@ -6,7 +6,6 @@ import (
 	"context"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	clientset "k8s.io/client-go/kubernetes"
 )
 
 type NodeService interface {
@@ -35,7 +34,6 @@ type SchedulerService interface {
 
 // Service cleans up
 type Service struct {
-	client       clientset.Interface
 	nodeService  NodeService
 	pvService    PersistentVolumeService
 	pvcService   PersistentVolumeClaimService
@@ -46,7 +44,6 @@ type Service struct {
 
 // NewResetService initializes Service.
 func NewResetService(
-	client clientset.Interface,
 	nodeService NodeService,
 	pvService PersistentVolumeService,
 	pvcService PersistentVolumeClaimService,
@@ -55,7 +52,6 @@ func NewResetService(
 	schedService SchedulerService,
 ) *Service {
 	return &Service{
-		client:       client,
 		nodeService:  nodeService,
 		pvService:    pvService,
 		pvcService:   pvcService,
