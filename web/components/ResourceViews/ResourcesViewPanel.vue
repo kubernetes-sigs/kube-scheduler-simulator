e3aa6de8f77f76a0e6502fad536bd332b26668ce
196Ikuchil <196thinline@gmail.com>
2022-02-23 20:15:18 +0900

    add: ResourcesViewPanel & RadioButton to select view

--- web/components/ResourceViews/ResourcesViewPanel.vue
+++ web/components/ResourceViews/ResourcesViewPanel.vue
@@ -1,31 +1,35 @@
 <template>
   <div>
-    <NodeList />
-    <UnscheduledPodList />
-    <PVList />
-    <PVCList />
-    <StorageClassList />
-    <PriorityClassList />
+    <ViewSelectRadioButton
+      v-model="selectedViewType"
+      :radio-labels="viewTypeLabels"
+    />
+    <div v-if="selectedViewType === 0">
+      <ResourcesList />
+    </div>
+    <div v-else-if="selectedViewType === 1">
+      <!-- TODO: -->
+    </div>
   </div>
 </template>
 
 <script lng="ts">
-import { defineComponent } from "@nuxtjs/composition-api";
-import NodeList from "~/components/ResourceViews/List/NodeList.vue";
-import UnscheduledPodList from "~/components/ResourceViews/List/UnscheduledPodList.vue";
-import PVList from "~/components/ResourceViews/List/PVList.vue";
-import PVCList from "~/components/ResourceViews/List/PVCList.vue";
-import StorageClassList from "~/components/ResourceViews/List/StorageClassList.vue";
-import PriorityClassList from "~/components/ResourceViews/List/PriorityClassList.vue";
+import { defineComponent, ref } from "@nuxtjs/composition-api";
+import ResourcesList from "./Lists/ResourcesList.vue";
+import ViewSelectRadioButton from "./ViewSeelectRadioButton.vue";
 
 export default defineComponent({
   components: {
-    NodeList,
-    UnscheduledPodList,
-    PVList,
-    PVCList,
-    StorageClassList,
-    PriorityClassList,
+    ResourcesList,
+    ViewSelectRadioButton,
+  },
+  setup() {
+    const viewTypeLabels = ["List", "Table"];
+    const selectedViewType = ref(0);
+    return {
+      viewTypeLabels,
+      selectedViewType,
+    };
   },
 });
 </script>
---
+++ web/components/ResourceViews/ViewSeelectRadioButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container fluid>
+    <v-radio-group v-model="viewTypeComputed" row>
+      <v-radio
+        v-for="(_, i) in radioLabels.length"
+        :key="i"
+        :label="radioLabels[i]"
+        :value="i"
+      ></v-radio>
+    </v-radio-group>
+  </v-container>
+</template>
+
+<script lng="ts">
+import { defineComponent, computed } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  props: {
+    radioLabels: {
+      type: Array,
+      default: () => [],
+    },
+    selectedViewType: {
+      type: Number,
+      default: 0,
+    },
+  },
+  emits: ["input"],
+  setup(props, { emit }) {
+    const viewTypeComputed = computed({
+      get: () => props.selectedViewType,
+      set: (newValue) => emit("input", newValue),
+    });
+    return {
+      viewTypeComputed,
+    };
+  },
+});
+</script>
