e379f73af5c244cbacc285877e79b2c7ad6fb36c
196Ikuchil <196thinline@gmail.com>
2022-02-27 01:59:05 +0900

    refactor:split file of schedulerconfiguration edit button

--- web/components/TopBar/TopBar.vue
+++ web/components/TopBar/TopBar.vue
@@ -1,16 +1,14 @@
 <template>
   <v-sheet class="transparent">
-    <v-btn class="ma-2" @click="onClick(buttonName[0])">
-      <v-icon> mdi-cog </v-icon>
-    </v-btn>
+    <SchedulerConfigurationEditButton />
     <ExportButton />
     <ImportButton />
   </v-sheet>
 </template>
 
 <script lang="ts">
-import { inject, defineComponent } from "@nuxtjs/composition-api";
-import SchedulerConfigurationStoreKey from "../StoreKey/SchedulerConfigurationStoreKey";
+import { defineComponent } from "@nuxtjs/composition-api";
+import SchedulerConfigurationEditButton from "./SchedulerConfigurationEditButton.vue";
 import ExportButton from "./ExportButton.vue";
 import ImportButton from "./ImportButton.vue";
 
@@ -18,25 +16,7 @@ export default defineComponent({
   components: {
     ImportButton,
     ExportButton,
-  },
-  setup() {
-    const schedulerconfigurationstore = inject(SchedulerConfigurationStoreKey);
-    if (!schedulerconfigurationstore) {
-      throw new Error(`${SchedulerConfigurationStoreKey} is not provided`);
-    }
-
-    const buttonName = ["SchedulerConfiguration"];
-    const onClick = (bn: string) => {
-      switch (bn) {
-        case "SchedulerConfiguration":
-          schedulerconfigurationstore.fetchSelected();
-          break;
-      }
-    };
-    return {
-      onClick,
-      buttonName,
-    };
+    SchedulerConfigurationEditButton,
   },
 });
 </script>
