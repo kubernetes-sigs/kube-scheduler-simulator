e063005d9242886db4313c53431ce0c48d470913
matthewygf <yyygggfff@hotmail.com>
2022-02-28 10:19:00 +0000

    WIP - remove unecessary shutdown fn

--- simulator.go
+++ simulator.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"os"
 	"os/signal"
 	"syscall"
@@ -30,11 +31,12 @@ func startSimulator() error {
 		return xerrors.Errorf("get config: %w", err)
 	}
 
-	restclientCfg, apiShutdown, err := k8sapiserver.StartAPIServer(cfg.KubeAPIServerURL, cfg.EtcdURL)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	restclientCfg, err := k8sapiserver.StartAPIServer(cfg.KubeAPIServerURL, cfg.EtcdURL, ctx.Done())
 	if err != nil {
 		return xerrors.Errorf("start API server: %w", err)
 	}
-	defer apiShutdown()
 
 	client := clientset.NewForConfigOrDie(restclientCfg)
 
