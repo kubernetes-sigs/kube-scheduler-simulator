8e04159de710c3ed86a3a748d876bbcabbda8cc0
196Ikuchil <196thinline@gmail.com>
2022-04-18 18:26:36 +0900

    fix:remove ``

--- scheduler/plugin/wrappedplugin.go
+++ scheduler/plugin/wrappedplugin.go
@@ -41,7 +41,7 @@ type NormalizeScorePluginExtender interface {
 	// If BeforeNormalizeScore returns non-success status, the simulator plugin doesn't run the NormalizeScore method of the original plugin and return that status.
 	BeforeNormalizeScore(ctx context.Context, state *framework.CycleState, pod *v1.Pod, scores framework.NodeScoreList) *framework.Status
 	// AfterNormalizeScore is a function that runs after the NormalizeScore method of the original plugin.
-	// A NormalizeScore of the simulator plugins finally returns the status returned from `AfterNormalizeScore`.
+	// A NormalizeScore of the simulator plugins finally returns the status returned from AfterNormalizeScore.
 	AfterNormalizeScore(ctx context.Context, state *framework.CycleState, pod *v1.Pod, scores framework.NodeScoreList, normalizeScoreResult *framework.Status) *framework.Status
 }
 
