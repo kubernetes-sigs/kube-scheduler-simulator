1e69ad1e7f7cafb29f4380b6a5a88e1aae9c7221
196Ikuchil <196thinline@gmail.com>
2022-05-06 23:56:49 +0900

    test:new testcase of multiple namespaces

--- node/node_test.go
+++ node/node_test.go
@@ -260,6 +260,59 @@ func TestService_DeleteCollection(t *testing.T) {
 			},
 			wantErr: true,
 		},
+		{
+			name: "delete nodes with multiple existing namespaced pods",
+			preparePodServiceMockFn: func(m *mock_node.MockPodService) {
+				m.EXPECT().DeleteCollection(gomock.Any(), "default1", metav1.ListOptions{
+					FieldSelector: "spec.nodeName=node1",
+				}).Return(nil)
+				m.EXPECT().DeleteCollection(gomock.Any(), "default2", metav1.ListOptions{
+					FieldSelector: "spec.nodeName=node1",
+				}).Return(nil)
+				m.EXPECT().DeleteCollection(gomock.Any(), "default3", metav1.ListOptions{
+					FieldSelector: "spec.nodeName=node1",
+				}).Return(errors.New("error"))
+			},
+			prepareFakeClientSetFn: func() *fake.Clientset {
+				c := fake.NewSimpleClientset()
+				c.CoreV1().Namespaces().Create(context.Background(), &corev1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "default1",
+					},
+				}, metav1.CreateOptions{})
+				c.CoreV1().Namespaces().Create(context.Background(), &corev1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "default2",
+					},
+				}, metav1.CreateOptions{})
+				c.CoreV1().Pods("default1").Create(context.Background(), &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "pod1",
+					},
+					Spec: corev1.PodSpec{
+						NodeName: "node1",
+					},
+				}, metav1.CreateOptions{})
+				c.CoreV1().Pods("default2").Create(context.Background(), &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "pod2",
+					},
+					Spec: corev1.PodSpec{
+						NodeName: "node1",
+					},
+				}, metav1.CreateOptions{})
+				c.CoreV1().Nodes().Create(context.Background(), &corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "node1",
+					},
+				}, metav1.CreateOptions{})
+				return c
+			},
+			lopts: metav1.ListOptions{
+				FieldSelector: "spec.nodeName!=",
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		tt := tt
