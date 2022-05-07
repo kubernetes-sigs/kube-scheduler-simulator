131c2b9def7284490662ff48d7fccb6deb1edf5d
196Ikuchil <196Ikuchil@gmail.com>
2022-05-07 04:21:35 +0000

    fix:call api directory& delete unnecessary files

--- web/components/StoreKey/ResetStoreKey.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionKey } from "@nuxtjs/composition-api";
-import { ResetStore } from "~/store/reset";
-
-const ResetStoreKey: InjectionKey<ResetStore> = Symbol("ResetStore");
-export default ResetStoreKey;
