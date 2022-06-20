04d85fcfccc12261cdc8a31a81198da4a58caa5f
Agustina Barbetta <agustina.barbetta@gmail.com>
2022-06-20 20:54:12 -0300

    add deleteCollectionReaction to fake client

--- priorityclass/priorityclass_test.go
+++ priorityclass/priorityclass_test.go
@@ -2,13 +2,51 @@ package priorityclass
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	schedulingv1 "k8s.io/api/scheduling/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/fake"
+	k8sTesting "k8s.io/client-go/testing"
 )
+
+// deleteCollectionReaction returns a ReactionFunc that applies DeleteCollectionAction
+// to the given tracker.
+func deleteCollectionReaction(tracker k8sTesting.ObjectTracker) k8sTesting.ReactionFunc {
+	return func(action k8sTesting.Action) (bool, runtime.Object, error) {
+		ns := action.GetNamespace()
+		gvr := action.GetResource()
+		gvk := action.GetResource().GroupVersion().WithKind("PriorityClass")
+
+		switch action := action.(type) {
+
+		case k8sTesting.DeleteCollectionActionImpl:
+			obj, err := tracker.List(gvr, gvk, ns)
+			list := obj.(*schedulingv1.PriorityClassList)
+			for _, class := range list.Items {
+				nameField := fields.Set{"metadata.name": class.Name}
+				if action.GetListRestrictions().Fields.Matches(nameField) {
+					err := tracker.Delete(gvr, ns, class.Name)
+					if err != nil {
+						return true, nil, err
+					}
+				}
+			}
+			return true, obj, err
+
+		default:
+			return false, nil, fmt.Errorf("no reaction implemented for %s", action)
+		}
+	}
+}
+
+func addReactorForPriorityClasses(c *fake.Clientset) {
+	c.AddReactor("delete-collection", "priorityclasses", deleteCollectionReaction(c.Tracker()))
+}
 
 func createDefaultPriorityClasses(c *fake.Clientset) {
 	c.SchedulingV1().PriorityClasses().Create(context.Background(), &schedulingv1.PriorityClass{
@@ -49,6 +87,7 @@ func TestService_DeleteCollection(t *testing.T) {
 			name: "delete all priorityclasses",
 			prepareFakeClientSetFn: func() *fake.Clientset {
 				c := fake.NewSimpleClientset()
+				addReactorForPriorityClasses(c)
 				createDefaultPriorityClasses(c)
 				c.SchedulingV1().PriorityClasses().Create(context.Background(), &schedulingv1.PriorityClass{
 					ObjectMeta: metav1.ObjectMeta{
@@ -81,6 +120,7 @@ func TestService_DeleteCollection(t *testing.T) {
 			name: "delete all priorityclasses, only default classes",
 			prepareFakeClientSetFn: func() *fake.Clientset {
 				c := fake.NewSimpleClientset()
+				addReactorForPriorityClasses(c)
 				createDefaultPriorityClasses(c)
 				return c
 			},
@@ -94,6 +134,7 @@ func TestService_DeleteCollection(t *testing.T) {
 			name: "delete all priorityclasses with fieldSelector",
 			prepareFakeClientSetFn: func() *fake.Clientset {
 				c := fake.NewSimpleClientset()
+				addReactorForPriorityClasses(c)
 				createDefaultPriorityClasses(c)
 				c.SchedulingV1().PriorityClasses().Create(context.Background(), &schedulingv1.PriorityClass{
 					ObjectMeta: metav1.ObjectMeta{
