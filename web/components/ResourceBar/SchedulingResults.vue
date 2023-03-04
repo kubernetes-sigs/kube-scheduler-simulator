295bc58d6aa1a6b264f32c078e9c2f711088e9c8
196Ikuchil <22634362+196Ikuchil@users.noreply.github.com>
2023-03-03 19:50:56 -0800

    fix:bug #179#180 (#240)

--- web/components/ResourceBar/SchedulingResults.vue
+++ web/components/ResourceBar/SchedulingResults.vue
@@ -1,38 +1,38 @@
 <template>
   <v-expansion-panels accordion multiple>
-    <v-expansion-panel v-if="filterTableData.length > 1">
+    <v-expansion-panel v-if="filterTableData().length > 1">
       <v-expansion-panel-header> Filter </v-expansion-panel-header>
       <v-expansion-panel-content>
         <v-data-table
           dense
-          :headers="filterTableHeader"
-          :items="filterTableData"
+          :headers="filterTableHeader()"
+          :items="filterTableData()"
           item-key="Node"
         >
         </v-data-table>
       </v-expansion-panel-content>
     </v-expansion-panel>
-    <v-expansion-panel v-if="scoreTableData.length > 1">
+    <v-expansion-panel v-if="scoreTableData().length > 1">
       <v-expansion-panel-header> Score </v-expansion-panel-header>
       <v-expansion-panel-content>
         <v-data-table
           dense
-          :headers="scoreTableHeader"
-          :items="scoreTableData"
+          :headers="scoreTableHeader()"
+          :items="scoreTableData()"
           item-key="Node"
         >
         </v-data-table>
       </v-expansion-panel-content>
     </v-expansion-panel>
-    <v-expansion-panel v-if="finalscoreTableData.length > 1">
+    <v-expansion-panel v-if="finalscoreTableData().length > 1">
       <v-expansion-panel-header>
         Final Score (Normalized + Applied plugin weight)
       </v-expansion-panel-header>
       <v-expansion-panel-content>
         <v-data-table
           dense
-          :headers="finalscoreTableHeader"
-          :items="finalscoreTableData"
+          :headers="finalscoreTableHeader()"
+          :items="finalscoreTableData()"
           item-key="Node"
         >
         </v-data-table>
@@ -41,86 +41,84 @@
   </v-expansion-panels>
 </template>
 <script lang="ts">
-import {
-  ref,
-  defineComponent,
-  inject,
-  computed,
-  watch,
-} from "@nuxtjs/composition-api";
+import { V1Pod } from "@kubernetes/client-node";
+import { defineComponent } from "@nuxtjs/composition-api";
 import { extractTableHeader, schedulingResultToTableData } from "../lib/util";
-import PodStoreKey from "../StoreKey/PodStoreKey";
 
 export default defineComponent({
-  setup() {
-    const podstore = inject(PodStoreKey);
-    if (!podstore) {
-      throw new Error(`${PodStoreKey.description} is not provided`);
-    }
-
-    // scheduling results
-    const filterTableHeader = ref(
-      [] as Array<{
-        text: string;
-        value: string;
-      }>
-    );
-    const filterTableData = ref(
-      [] as Array<{ [name: string]: string | number }>
-    );
-    const scoreTableHeader = ref(
-      [] as Array<{
-        text: string;
-        value: string;
-      }>
-    );
-    const scoreTableData = ref(
-      [] as Array<{ [name: string]: string | number }>
-    );
-    const finalscoreTableHeader = ref(
-      [] as Array<{
-        text: string;
-        value: string;
-      }>
-    );
-    const finalscoreTableData = ref(
-      [] as Array<{ [name: string]: string | number }>
-    );
-
+  props: {
+    selected: {
+      type: Object,
+    },
+  },
+  
+  setup(props) {
     const filterResultAnnotationKey = "scheduler-simulator/filter-result";
     const scoreResultAnnotationKey = "scheduler-simulator/score-result";
     const finalScoreResultAnnotationKey =
       "scheduler-simulator/finalscore-result";
 
-    const pod = computed(() => podstore.selected);
-    watch(pod, () => {
-      if (pod.value?.item.metadata?.annotations) {
-        if (scoreResultAnnotationKey in pod.value.item.metadata.annotations) {
-          var score = JSON.parse(
-            pod.value?.item.metadata?.annotations[scoreResultAnnotationKey]
-          );
+    // scheduling results
+    const filterTableHeader = ():Array<{text: string;value: string;}> => {
+      const p = props.selected as V1Pod;
+      if (p.metadata?.annotations) {
+        if (filterResultAnnotationKey in p.metadata.annotations) {
+          return extractTableHeader(JSON.parse(
+            p.metadata.annotations[filterResultAnnotationKey]
+          ));
         }
-        if (
-          finalScoreResultAnnotationKey in pod.value.item.metadata.annotations
-        ) {
-          var finalscore = JSON.parse(
-            pod.value?.item.metadata?.annotations[finalScoreResultAnnotationKey]
-          );
-        }
-        if (filterResultAnnotationKey in pod.value.item.metadata.annotations) {
-          var filter = JSON.parse(
-            pod.value?.item.metadata?.annotations[filterResultAnnotationKey]
-          );
-        }
-
-        filterTableHeader.value = extractTableHeader(filter);
-        filterTableData.value = schedulingResultToTableData(filter);
-        scoreTableHeader.value = extractTableHeader(score);
-        scoreTableData.value = schedulingResultToTableData(score);
-        finalscoreTableHeader.value = extractTableHeader(finalscore);
-        finalscoreTableData.value = schedulingResultToTableData(finalscore);
       }
-    });
+      return [];
+    }
+
+    const filterTableData = ():Array<{ [name: string]: string | number }> => {
+      const p = props.selected as V1Pod;
+      if (p.metadata?.annotations) {
+        if (filterResultAnnotationKey in p.metadata.annotations) {
+          return schedulingResultToTableData(JSON.parse(p.metadata.annotations[filterResultAnnotationKey]));
+        }
+      }
+      return [];
+    };
+
+    const scoreTableHeader = ():Array<{text: string;value: string;}> => {
+      const p = props.selected as V1Pod;
+      if (p.metadata?.annotations) {
+        if (scoreResultAnnotationKey in p.metadata.annotations) {
+          return extractTableHeader(JSON.parse(p.metadata.annotations[scoreResultAnnotationKey]));
+        }
+      }
+      return [];
+    };
+
+    const scoreTableData = ():Array<{ [name: string]: string | number }> => {
+      const p = props.selected as V1Pod;
+      if (p.metadata?.annotations) {
+        if (scoreResultAnnotationKey in p.metadata.annotations) {
+          return schedulingResultToTableData(JSON.parse(p.metadata.annotations[scoreResultAnnotationKey]));
+        }
+      }
+      return [];
+    };
+
+    const finalscoreTableHeader = ():Array<{text: string;value: string;}> => {
+      const p = props.selected as V1Pod;
+      if (p.metadata?.annotations) {
+        if (finalScoreResultAnnotationKey in p.metadata.annotations) {
+          return extractTableHeader(JSON.parse(p.metadata.annotations[finalScoreResultAnnotationKey]));
+        }
+      }
+      return [];
+    };
+    const finalscoreTableData = ():Array<{ [name: string]: string | number }> => {
+      const p = props.selected as V1Pod;
+      if (p.metadata?.annotations) {
+        if (finalScoreResultAnnotationKey in p.metadata.annotations) {
+          return schedulingResultToTableData(JSON.parse(p.metadata.annotations[finalScoreResultAnnotationKey]));
+        }
+      }
+      return [];
+    };
 
     return {
       filterTableHeader,
